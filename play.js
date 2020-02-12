85d5c4dd11d50a55d1c7dcfcacc1fd8f07306772
Wares <fazelyar.wares@gmail.com>
2020-02-12 20:56:25 +0000

    set up exit fucntion using control + c

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(key) {
+  if (key === ('\u0003')) {
+    process.exit(console.log('Snake game over'));
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (handleUserInput)) 
+
+  return stdin;
+}
+
+setupInput();
