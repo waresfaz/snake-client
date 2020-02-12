c53c51de83ff1681b4a15f5b1b29a6d49afea393
Wares <fazelyar.wares@gmail.com>
2020-02-12 18:45:05 +0000

    seperated connect function in client.js and exported to play.js

--- play.js
+++ play.js
@@ -1,23 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.0.236',
-    port: 50541
-  });
-
-  conn.on('data', (data) => {
-    console.log(data)
-  })
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
